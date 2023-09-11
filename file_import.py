bdb923c8fa647c1cd47fdb8767ed4745492785e4
Jon Slotved <jonsslotved@gmail.com>
2023-09-11 08:46:17 +0200

    requirements update: openpyxl

--- file_import.py
+++ file_import.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 # creating a dataframe from a csv file
-df = pd.read_csv('ammo_stats.csv')
+df = pd.read_excel('ammo_stats.xlsx')
 
 # view the first 5 rows of the dataframe
 print(df.head(100))

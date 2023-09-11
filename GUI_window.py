15dc681b259bf7298052af5f0123284c9e0ddef5
Jon Slotved <jonsslotved@gmail.com>
2023-09-11 09:14:52 +0200

    nothing important

--- GUI_window.py
+++ GUI_window.py
@@ -4,12 +4,11 @@ import tkinter as tk
 # create a window
 GUI = tk.Tk()
 
-print(tk.TkVersion)
 # set the geometry of window
 GUI.geometry("550x350")
 
 # set the background color of GUI window
-GUI.configure(background="blue")
+GUI.configure(background="dark gray")
 
 # set the title of GUI window
 GUI.title("test")
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # EFT_query_project
  A project where I will create a way to make queries on an ammo sheet from EFT
+ see requirements.txt for requirements...
+ 
